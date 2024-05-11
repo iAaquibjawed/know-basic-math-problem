205f09e85454eef133ea6383f34163b9b7643c34
iAaquibjawed <mallickaaquib@gmail.com>
2024-05-11 15:52:51 +0530

    another approach using number

--- digit-count.rb
+++ digit-count.rb
@@ -9,3 +9,18 @@ end
 
 
 puts count_digits(1234) # 4
+
+
+
+
+def digit_count(number)
+    count = 0 
+    while number>0
+        byebug
+        number = number/10
+        count += 1
+    end
+    return count
+end
+
+puts digit_count(1234) # 4
